c28ffa256b298fa80d3d31575c5c0e772141601c
Ravdes <wiktornowak204@gmail.com>
2023-12-13 18:35:12 +0100

    First commit

---
+++ src/main/java/com/example/authenticatedsignup/AuthenticatedSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticatedsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatedSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatedSignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authenticatedsignup/AuthenticatedSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticatedsignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
